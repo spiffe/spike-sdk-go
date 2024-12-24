035c0dd4828caf964dabe9416ce9522f089d2256
Volkan Özçelik <me@volkan.io>
2024-12-23 18:08:36 -0800

    Remove redundant error codes.
    Signed-off-by: Volkan Özçelik <me@volkan.io>

--- api/entity/data/error_code.go
+++ api/entity/data/error_code.go
@@ -7,14 +7,22 @@ package data
 type ErrorCode string
 
 const ErrBadInput = ErrorCode("bad_request")
-const ErrServerFault = ErrorCode("server_fault")
-const ErrUnauthorized = ErrorCode("unauthorized")
+
+// ErrBadInput can be used for all of these.
+//const ErrInvalidName = ErrorCode("invalid_name")
+//const ErrInvalidSpiffeIdPattern = ErrorCode("invalid_spiffe_id_pattern")
+//const ErrInvalidPermission = ErrorCode("invalid_permission")
+//const ErrInvalidPathPattern = ErrorCode("invalid_path_pattern")
+
+// Use ErrInternal instead.
+// const ErrServerFault = ErrorCode("server_fault")
+
 const ErrInternal = ErrorCode("internal_error")
+
+const ErrUnauthorized = ErrorCode("unauthorized")
 const ErrLowEntropy = ErrorCode("low_entropy")
+
 const ErrAlreadyInitialized = ErrorCode("already_initialized")
+
 const ErrNotFound = ErrorCode("not_found")
 const ErrSuccess = ErrorCode("success")
-const ErrInvalidName = ErrorCode("invalid_name")
-const ErrInvalidSpiffeIdPattern = ErrorCode("invalid_spiffe_id_pattern")
-const ErrInvalidPermission = ErrorCode("invalid_permission")
-const ErrInvalidPathPattern = ErrorCode("invalid_path_pattern")
