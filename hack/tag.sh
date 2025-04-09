bc5f5deafa9495fddeeb8402e694bfacd72501d0
Volkan Özçelik <me@volkan.io>
2025-04-08 22:40:02 -0700

    Add hack folder.
    Signed-off-by: Volkan Özçelik <me@volkan.io>

---
+++ hack/tag.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#    \\ SPIKE: Secure your secrets with SPIFFE.
+#  \\\\\ Copyright 2024-present SPIKE contributors.
+# \\\\\\\ SPDX-License-Identifier: Apache-2.0
+
+VERSION="v0.5.5"
+
+git tag -s "$VERSION" -m "$VERSION"
+git push oigin --tags
