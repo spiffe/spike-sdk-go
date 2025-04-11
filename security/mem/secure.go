052208495daae5a9f1a19723669cbf9730db6dd4
Volkan Özçelik <me@volkan.io>
2025-04-10 20:43:34 -0700

    Add memory locking.
    Signed-off-by: Volkan Özçelik <me@volkan.io>

--- security/mem/secure.go
+++ security/mem/secure.go
@@ -8,6 +8,7 @@ package mem
 import (
 	"crypto/rand"
 	"runtime"
+	"syscall"
 	"unsafe"
 )
 
@@ -175,4 +176,20 @@ func ClearBytes(b []byte) {
 
 	// Make sure the data is actually wiped before gc has time to interfere
 	runtime.KeepAlive(b)
+}
+
+// Lock attempts to lock the process memory to prevent swapping.
+// Returns true if successful, false if not supported or failed.
+func Lock() bool {
+	// `mlock` is only available on Unix-like systems
+	if runtime.GOOS == "windows" {
+		return false
+	}
+
+	// Attempt to lock all current and future memory
+	if err := syscall.Mlockall(syscall.MCL_CURRENT | syscall.MCL_FUTURE); err != nil {
+		return false
+	}
+
+	return true
 }
