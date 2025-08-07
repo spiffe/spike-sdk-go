dd005d43479142b07c91c715202cf1b8d5ae1312
Volkan Özçelik <me@volkan.io>
2025-08-06 18:25:49 -0700

    version update

--- hack/tag.sh
+++ hack/tag.sh
@@ -4,7 +4,7 @@
 #  \\\\\ Copyright 2024-present SPIKE contributors.
 # \\\\\\\ SPDX-License-Identifier: Apache-2.0
 
-VERSION="v0.5.19"
+VERSION="v0.5.20"
 
 git tag -s "$VERSION" -m "$VERSION"
 git push origin --tags
