290ad2af0baebe6e0c7bf49da615894de0000b1e
Mert Şişmanoğlu <mertssmnoglu@gmail.com>
2025-08-11 23:51:53 +0300

    chore(lint): add lint script for golangci-lint
    Signed-off-by: Mert Şişmanoğlu <mertssmnoglu@gmail.com>

---
+++ hack/lint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#    \\ SPIKE: Secure your secrets with SPIFFE. — https://spike.ist/
+#  \\\\\ Copyright 2024-present SPIKE contributors.
+# \\\\\\\ SPDX-License-Identifier: Apache-2.0
+
+go run github.com/golangci/golangci-lint/cmd/golangci-lint run -v
+# '-v' flag is used for verbose output
