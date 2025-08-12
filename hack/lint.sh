535f6ffd6891cc8b27b15d5ae6adb0b545042df7
Mert Şişmanoğlu <mertssmnoglu@gmail.com>
2025-08-12 14:04:26 +0300

    fix(lint): remove obsolete lint script
    Signed-off-by: Mert Şişmanoğlu <mertssmnoglu@gmail.com>

--- hack/lint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#    \\ SPIKE: Secure your secrets with SPIFFE. — https://spike.ist/
-#  \\\\\ Copyright 2024-present SPIKE contributors.
-# \\\\\\\ SPDX-License-Identifier: Apache-2.0
-
-go run github.com/golangci/golangci-lint/cmd/golangci-lint@latest run -v
-# '-v' flag is used for verbose output
