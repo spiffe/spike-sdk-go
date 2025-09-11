41c9377aeb86574b7c4e2d5984d00b607ee97cdb
Volkan Özçelik <me@volkan.io>
2025-09-10 20:50:50 -0700

    add other constants.
    Signed-off-by: Volkan Özçelik <me@volkan.io>

---
+++ config/auth/auth.go
@@ -0,0 +1,9 @@
+//    \\ SPIKE: Secure your secrets with SPIFFE. — https://spike.ist/
+//  \\\\\ Copyright 2024-present SPIKE contributors.
+// \\\\\\\ SPDX-License-Identifier: Apache-2.0
+
+package auth
+
+const PathSystemPolicyAccess = "spike/system/acl"
+const PathSystemCipherEncrypt = "spike/system/cipher/encrypt"
+const PathSystemCipherDecrypt = "spike/system/cipher/decrypt"
