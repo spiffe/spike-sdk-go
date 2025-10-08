94be48070e5f6e5550666a0b74fa400c04227106
Volkan Özçelik <me@volkan.io>
2025-10-08 08:45:41 -0700

    Entities about bootstrap verification.
    Signed-off-by: Volkan Özçelik <me@volkan.io>

--- security/mem/lock.go
+++ security/mem/lock.go
@@ -7,14 +7,20 @@
 // Package mem provides utilities for secure mem operations.
 package mem
 
-import "syscall"
+import (
+	"syscall"
+
+	"github.com/spiffe/spike-sdk-go/log"
+)
 
 // Lock attempts to lock the process memory to prevent swapping.
 // Returns true if successful, false if not supported or failed.
 func Lock() bool {
+	const fName = "Lock"
 	// Attempt to lock all current and future memory
 	if err := syscall.Mlockall(
 		syscall.MCL_CURRENT | syscall.MCL_FUTURE); err != nil {
+		log.Log().Warn(fName, "msg", "Failed to lock memory", "err", err.Error())
 		return false
 	}
 
