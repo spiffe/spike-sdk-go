bd019cf770715b9230a5109c7089cff95c32ab25
Volkan Özçelik <me@volkan.io>
2025-11-15 19:05:39 -0800

    add deletion success code.
    Signed-off-by: Volkan Özçelik <me@volkan.io>

--- hack/tag.sh
+++ hack/tag.sh
@@ -4,7 +4,7 @@
 #  \\\\\ Copyright 2024-present SPIKE contributors.
 # \\\\\\\ SPDX-License-Identifier: Apache-2.0
 
-VERSION="v0.12.14"
+VERSION="v0.12.15"
 
 git tag -s "$VERSION" -m "$VERSION"
 git push origin --tags
