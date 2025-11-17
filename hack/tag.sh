f492fa248ffae1ec36a3660d729177d986255511
Volkan Özçelik <me@volkan.io>
2025-11-17 12:58:06 -0800

    more error codes.
    Signed-off-by: Volkan Özçelik <me@volkan.io>

--- hack/tag.sh
+++ hack/tag.sh
@@ -4,7 +4,7 @@
 #  \\\\\ Copyright 2024-present SPIKE contributors.
 # \\\\\\\ SPDX-License-Identifier: Apache-2.0
 
-VERSION="v0.12.39"
+VERSION="v0.12.40"
 
 git tag -s "$VERSION" -m "$VERSION"
 git push origin --tags
